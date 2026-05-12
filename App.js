3cb125a7cb68b559a3c768f7850d30d8e134a892
Isabelly Carvalho Piccinato <isabellpiccinato@gmail.com>
2026-05-12 17:53:57 -0300

    commit 1

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image} from 'react-native';
+import Feather from '@expo/vector-icons/Feather';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+        {/* inicio da header */}
+
+        <View style = {styles.viewHeader}>
+        <Feather name="menu" size={24} color="white" />
+          <Text style = {styles.textHeader}> TECFILMES </Text>
+          <TouchableOpacity>
+
+          </TouchableOpacity>
+
+
+        </View>
+
+
+        {/* inicio da barra pesquisa */}
+
+        <View style = {styles.conteinerSearch}>
+
+        <TextInput
+        placeholder='Digite o filme que deseja buscar'
+        ></TextInput>
+
+        </View>
+       
+        {/* inicio do banner */}
+
     </View>
   );
 }
@@ -13,8 +38,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(125, 188, 252)',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+
+  viewHeader: {
+    flexDirection: 'row',
+    width: "90%",
+    alignItems: 'center',
+    marginTop:10,
+    justifyContent: 'space-between'
+  },
+
+  textHeader:{
+    fontSize:22,
+    color:'white',
+    fontWeight:'bold'
+  },
+
+  conteinerSearch: {
+    marginTop:20,
+    width:"90%",
+    backgroundColor:'white',
+    borderRadius:5,
+    padding:8,
+  }
+
+  
 });
