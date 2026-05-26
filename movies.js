d01b8390bbcfbd9ebc097bd0a6fa9f7c8da2e318
Isabelly Carvalho Piccinato <isabellpiccinato@gmail.com>
2026-05-26 17:39:29 -0300

    commit 6

---
+++ movies.js
@@ -0,0 +1,65 @@
+const DATA =
+[
+{
+    id: '01',
+    nome: 'Avatar: O Caminho da Água',
+    nota: '9.9',
+    imagem: '/img/68079_avatar-o-caminho-da-agua.webp',
+},
+{
+    id: '02',
+    nome: 'Pantera Negra: Wakanda Para Sempre',
+    nota: '10.0',
+    imagem: '/img/68079_pantera-negra-wakanda-para-sempre.webp',
+},
+{
+    id: '03',
+    nome: 'Invocação do Mal',
+    nota: '7.8',
+    imagem: '/img/68079_invocacao-do-mal.webp',
+},
+{
+    id: '04',
+    nome: 'Trilogia Rua do Medo',
+    nota: '5.3',
+    imagem: '/img/68079_trilogia-rua-do-medo.webp',
+},
+{
+    id: '05',
+    nome: 'Mistério em Paris',
+    nota: '3.5',
+    imagem: '/img/68079_misterio-em-paris.webp',
+},
+{
+    id: '06',
+    nome: 'A Filha Perdida',
+    nota: '6.8',
+    imagem: '/img/68079_a-filha-perdida-netflix-media-center.webp',
+},
+{
+    id: '07',
+    nome: 'Imperdoável ',
+    nota: '4.1',
+    imagem: '/img/68079_imperdoavel-kimberley-frenchnetflix--2021-.webp',
+},
+{
+    id: '08',
+    nome: 'Não Olhe para Cimae',
+    nota: '2.6',
+    imagem: '/img/68079_nao-olhe-pra-cima-niko-tavernisenetflix-.webp',
+},
+{
+    id: '09',
+    nome: 'Um Ninho Para Dois',
+    nota: '6.4',
+    imagem: '/img/68079_um-ninho-para-dois-netflix-media-center.webp',
+},
+{
+    id: '10',
+    nome: 'O Culpado',
+    nota: '9.8',
+    imagem: '/img/68079_o-culpado-netflix-media-center.webp',
+},
+
+
+]
